5754da7058709ebe03576a9dc9c7ccdb65317824
AlexSyvorotkin <aleks031994@gmail.com>
2025-04-26 16:49:20 +0300

    fix legacy

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./legacy/context/ThemeContext/ThemeContext.tsx";
 import { Provider } from "react-redux";
-import { LanguageProvider } from "./legacy/context/LanguageContext/LanguageContext.tsx";
-import store from "./legacy/redux/store.ts";
+import store from "./store/store.ts";
 
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
-      <ThemeProvider>
-        <LanguageProvider>
           <App />
-        </LanguageProvider>
-      </ThemeProvider>
     </Provider>
   </BrowserRouter>
 );
---
+++ src/store/store.ts
@@ -0,0 +1,42 @@
+import {configureStore} from "@reduxjs/toolkit";
+// import user from './userSlice/userSlice'
+// import TeamStatsControls from "./TeamStatsControls/TeamStatsControls";
+// import test from './TestingConnection/TestingConnection'
+// import interfaceState from './InterfaceSlice/InterfaceSlice'
+// import teamTabs from "./TeamTabsSlice/TeamTabsSlice";
+// import candleSliceNew from "./candle-slice/candle-slice";
+// import guidelineRoadmap from './GuidelineRoadmapSlice/GuidelineRoadmapSlice'
+// import tournamentSlice from "./tournament-slice/tournament-slice";
+// import { championshipInfoApi } from "../services/championships-api/championship-api";
+// import { candleApi } from "../services/candles-api/candle-api";
+// import { newsApi } from "../services/news-api/news-api";
+// import { newsTagsApi } from "../services/news-tags-api/news-tags-api";
+
+const store = configureStore({
+    reducer: {},
+    // reducer: {
+    //     tournamentSlice,
+    //     TeamStatsControls,
+    //     user,
+    //     test,
+    //     interfaceState,
+    //     teamTabs,
+    //     candleSliceNew,
+    //     guidelineRoadmap,
+    //     [championshipInfoApi.reducerPath]: championshipInfoApi.reducer,
+    //     [candleApi.reducerPath]: candleApi.reducer,
+    //     [newsApi.reducerPath]: newsApi.reducer,
+    //     [newsTagsApi.reducerPath]: newsTagsApi.reducer
+    // },
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    //     .concat(championshipInfoApi.middleware)
+    //     .concat(candleApi.middleware)
+    //     .concat(newsApi.middleware)
+    //     .concat(newsTagsApi.middleware)
+
+})
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
